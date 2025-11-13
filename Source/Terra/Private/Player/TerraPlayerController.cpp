c19ed329d44a983b898c229b118d10bcdeab9ccf
vib <vibrunazo@gmail.com>
2025-11-12 23:40:59 -0300

    add wasd movement

--- Source/Terra/Private/Player/TerraPlayerController.cpp
+++ Source/Terra/Private/Player/TerraPlayerController.cpp
@@ -3,3 +3,65 @@
 
 #include "Player/TerraPlayerController.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+ATerraPlayerController::ATerraPlayerController()
+{
+}
+
+void ATerraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ATerraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	// Add input mapping context
+	
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+
+	// Setup Action Bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATerraPlayerController::TriggeredMove);
+		EnhancedInputComponent->BindAction(Ability1Action, ETriggerEvent::Triggered, this, &ATerraPlayerController::TriggeredAbility1);
+		EnhancedInputComponent->BindAction(Ability2Action, ETriggerEvent::Triggered, this, &ATerraPlayerController::TriggeredAbility2);
+		EnhancedInputComponent->BindAction(Ability3Action, ETriggerEvent::Triggered, this, &ATerraPlayerController::TriggeredAbility3);
+		EnhancedInputComponent->BindAction(Ability4Action, ETriggerEvent::Triggered, this, &ATerraPlayerController::TriggeredAbility4);
+		
+	}
+}
+
+void ATerraPlayerController::TriggeredMove(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputVector = InputActionValue.Get<FVector2D>();
+
+	if (APawn* ControlledPawn = GetPawn())
+	{
+		// Forward/Backward Movement (World X-axis)
+		ControlledPawn->AddMovementInput(FVector(1.0f, 0.0f, 0.0f), InputVector.Y);
+		// Right/Left Movement (World Y-axis)
+		ControlledPawn->AddMovementInput(FVector(0.0f, 1.0f, 0.0f), InputVector.X);
+	}
+}
+
+void ATerraPlayerController::TriggeredAbility1(const FInputActionValue& InputActionValue)
+{
+}
+
+void ATerraPlayerController::TriggeredAbility2(const FInputActionValue& InputActionValue)
+{
+}
+
+void ATerraPlayerController::TriggeredAbility3(const FInputActionValue& InputActionValue)
+{
+}
+
+void ATerraPlayerController::TriggeredAbility4(const FInputActionValue& InputActionValue)
+{
+}
--- Source/Terra/Public/Player/TerraPlayerController.h
+++ Source/Terra/Public/Player/TerraPlayerController.h
@@ -3,9 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/PlayerController.h"
 #include "TerraPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
 /**
  * 
  */
@@ -14,4 +17,44 @@ class TERRA_API ATerraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+	
+public:
+	// Sets default values for this player controller's properties
+	ATerraPlayerController();
+
+	// Mapping Context
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+
+	// Input Action Move
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveAction;
+
+	// Input Action Ability 1
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* Ability1Action;
+
+	// Input Action Ability 2
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* Ability2Action;
+
+	// Input Action Ability 3
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* Ability3Action;
+
+	// Input Action Ability 4
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* Ability4Action;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	// Called to bind functionality to input
+	virtual void SetupInputComponent() override;
+
+	void TriggeredMove(const FInputActionValue& InputActionValue);
+	void TriggeredAbility1(const FInputActionValue& InputActionValue);
+	void TriggeredAbility2(const FInputActionValue& InputActionValue);
+	void TriggeredAbility3(const FInputActionValue& InputActionValue);
+	void TriggeredAbility4(const FInputActionValue& InputActionValue);
 };
