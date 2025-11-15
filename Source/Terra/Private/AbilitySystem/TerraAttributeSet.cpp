48bb24d00934d178ca179b8aa20fd8c6a1ec14a5
vib <vibrunazo@gmail.com>
2025-11-15 20:41:52 -0300

    add attributes to cpp

--- Source/Terra/Private/AbilitySystem/TerraAttributeSet.cpp
+++ Source/Terra/Private/AbilitySystem/TerraAttributeSet.cpp
@@ -3,3 +3,71 @@
 
 #include "AbilitySystem/TerraAttributeSet.h"
 
+#include "Net/UnrealNetwork.h"
+
+UTerraAttributeSet::UTerraAttributeSet()
+{
+	Health = 100.f;
+	MaxHealth = 100.f;
+	Mana = 50.f;
+	MaxMana = 100.f;
+	Strength = 10.f;
+	Agility = 10.f;
+	Intelligence = 10.f;
+	MoveSpeed = 100.f;
+	
+}
+
+void UTerraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(UTerraAttributeSet, Health);
+	DOREPLIFETIME(UTerraAttributeSet, MaxHealth);
+	DOREPLIFETIME(UTerraAttributeSet, Mana);
+	DOREPLIFETIME(UTerraAttributeSet, MaxMana);
+	DOREPLIFETIME(UTerraAttributeSet, Strength);
+	DOREPLIFETIME(UTerraAttributeSet, Agility);
+	DOREPLIFETIME(UTerraAttributeSet, Intelligence);
+	DOREPLIFETIME(UTerraAttributeSet, MoveSpeed);
+}
+
+void UTerraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTerraAttributeSet, Health, OldHealth);
+}
+
+void UTerraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTerraAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UTerraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTerraAttributeSet, Mana, OldMana);
+}
+
+void UTerraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTerraAttributeSet, MaxMana, OldMaxMana);
+}
+
+void UTerraAttributeSet::OnRep_Strength(const FGameplayAttributeData& OldStrength)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTerraAttributeSet, Strength, OldStrength);
+}
+
+void UTerraAttributeSet::OnRep_Agility(const FGameplayAttributeData& OldAgility)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTerraAttributeSet, Agility, OldAgility);
+}
+
+void UTerraAttributeSet::OnRep_Intelligence(const FGameplayAttributeData& OldIntelligence)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTerraAttributeSet, Intelligence, OldIntelligence);
+}
+
+void UTerraAttributeSet::OnRep_MoveSpeed(const FGameplayAttributeData& OldMoveSpeed)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTerraAttributeSet, MoveSpeed, OldMoveSpeed);
+}
--- Source/Terra/Public/AbilitySystem/TerraAttributeSet.h
+++ Source/Terra/Public/AbilitySystem/TerraAttributeSet.h
@@ -3,8 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "TerraAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +20,70 @@ UCLASS()
 class TERRA_API UTerraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+	
+public:
+	UTerraAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UTerraAttributeSet, Health)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UTerraAttributeSet, MaxHealth)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Mana)
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UTerraAttributeSet, Mana)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UTerraAttributeSet, MaxMana)
+	
+	// Strength, Agility, Intelligence
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Strength)
+	FGameplayAttributeData Strength;
+	ATTRIBUTE_ACCESSORS(UTerraAttributeSet, Strength)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Agility)
+	FGameplayAttributeData Agility;
+	ATTRIBUTE_ACCESSORS(UTerraAttributeSet, Agility)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Intelligence)
+	FGameplayAttributeData Intelligence;
+	ATTRIBUTE_ACCESSORS(UTerraAttributeSet, Intelligence)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_MoveSpeed)
+	FGameplayAttributeData MoveSpeed;
+	ATTRIBUTE_ACCESSORS(UTerraAttributeSet, MoveSpeed)
+	
+	
+	
+protected:
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+	
+	UFUNCTION()
+	virtual void OnRep_Mana(const FGameplayAttributeData& OldMana);
+	
+	UFUNCTION()
+	virtual void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
+	
+	UFUNCTION()
+	virtual void OnRep_Strength(const FGameplayAttributeData& OldStrength);
+	
+	UFUNCTION()
+	virtual void OnRep_Agility(const FGameplayAttributeData& OldAgility);
+	
+	UFUNCTION()
+	virtual void OnRep_Intelligence(const FGameplayAttributeData& OldIntelligence);
+	
+	UFUNCTION()
+	virtual void OnRep_MoveSpeed(const FGameplayAttributeData& OldMoveSpeed);
 	
 };
